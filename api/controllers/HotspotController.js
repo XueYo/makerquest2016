816a9d73dd6b0c3cc548ae327a6cc0073beca99f
XueYo <snow4eternity@gmail.com>
2016-03-21 17:14:34 -0700

    First model Yay

---
+++ api/controllers/HotspotController.js
@@ -0,0 +1,11 @@
+/**
+ * HotspotController
+ *
+ * @description :: Server-side logic for managing Hotspots
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Hotspot.js
@@ -0,0 +1,23 @@
+/**
+ * Hotspot.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+      zone: {
+          type: 'string',
+          enum: ['zoneOne','zoneTwo','zoneThree']
+         },
+      solveCount:  {
+          type: 'integer'
+      },
+      unqId: {
+      type: 'integer'
+  }
+  }
+};
+
