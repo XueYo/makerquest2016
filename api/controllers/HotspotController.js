a60288a7899f46b599e0a854a93b5026730d077a
dbhoot <dsbhoot@gmail.com>
2016-04-04 16:59:24 -0700

    sample

--- api/controllers/HotspotController.js
+++ api/controllers/HotspotController.js
@@ -7,5 +7,23 @@
 
 module.exports = {
 	
+	find: function(req,res){
+		var uniqId = req.param('id')
+		console.log(uniqId)
+
+		Hotspot.findOne({'unqId': uniqId}).exec(function(err, record)
+		{
+			if(err || !record) {
+				res.send("not found, try again")
+				
+			}
+			else{
+				res.send("found one!")
+			}
+		})
+
+	
+
+	}
 };
 
