471c3729081b777958320566785d329bc12c4786
XueYo <snow4eternity@gmail.com>
2016-04-25 17:31:11 -0700

    worked on User Controller

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,7 +5,21 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-module.exports = {
+module.exports = { 
+    
+    Progress: function(req,res){
+        var userid = req.param('user')
+        User.findOne({'username': userid}).exec(function(err,userobject)){
+                if(err || !userobject){
+                    res.send('user not found')
+                        }
+                else{
+                    res.send(userobject.accomplished.length)
+                }
+        
+                                             }
+    }
+    
 	
 };
 
